4a7e1067329c7eafcdd7b503ffff7f5f7202c0e0
danielprofili <dprofili@gmail.com>
2025-06-15 13:41:57 -0400

    fix broken stuff

--- .emacs.d/custom.el
+++ .emacs.d/custom.el
@@ -4,7 +4,10 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(telephone-line highlight-indent-guides ample-theme org-ref org-evil ox-twbs auctex benchmark-init disable-mouse yasnippet-snippets yasnippet evil-surround avy company hydra helm evil-better-visual-line)))
+   '(ample-theme auctex benchmark-init company delight disable-mouse
+                 evil-better-visual-line evil-surround helm
+                 highlight-indent-guides org-evil org-ref ox-twbs
+                 telephone-line yasnippet-snippets)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
