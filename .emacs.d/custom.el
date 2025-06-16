bb37330b08fc35ba076d6759820e8186aa95eab1
danielprofili <dprofili@gmail.com>
2025-06-16 16:14:37 -0400

    dired flags

--- .emacs.d/custom.el
+++ .emacs.d/custom.el
@@ -3,6 +3,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(dired-listing-switches "-alF --group-directories-first")
  '(evil-undo-system 'undo-redo)
  '(org-startup-folded t))
 (custom-set-faces
