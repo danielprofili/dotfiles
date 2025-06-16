46710ba4a91b53a7a454dbada5997d3a95394995
danielprofili <dprofili@gmail.com>
2025-06-16 10:41:42 -0400

    add emacs

---
+++ .emacs.d/custom.el
@@ -0,0 +1,21 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(org-startup-folded t))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(org-document-title ((t (:inherit default :weight bold :height 1.5 :underline nil))))
+ '(org-ellipsis ((t (:underline nil))))
+ '(org-level-1 ((t (:inherit default :weight bold :height 1.75 :foreground "DarkOrchid1"))))
+ '(org-level-2 ((t (:inherit default :weight bold :height 1.5 :foreground "DarkOrange"))))
+ '(org-level-3 ((t (:inherit default :weight bold :height 1.25 :foreground "SeaGreen2"))))
+ '(org-level-4 ((t (:inherit default :weight bold :height 1.1 :foreground "goldenrod"))))
+ '(org-level-5 ((t (:inherit default :weight bold))))
+ '(org-level-6 ((t (:inherit default :weight bold))))
+ '(org-level-7 ((t (:inherit default :weight bold))))
+ '(org-level-8 ((t (:inherit default :weight bold)))))
