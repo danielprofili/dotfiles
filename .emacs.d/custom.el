28f095ed38bfef89ca9ee206ec0479d0ae68c111
danielprofili <dprofili@gmail.com>
2025-06-21 21:46:51 -0400

    updates for broken stuf

--- .emacs.d/custom.el
+++ .emacs.d/custom.el
@@ -3,11 +3,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   '(ample-theme auctex benchmark-init company delight disable-mouse
-                 evil-better-visual-line evil-surround helm
-                 highlight-indent-guides org-evil org-ref ox-twbs
-                 telephone-line yasnippet-snippets)))
+ '(package-selected-packages nil))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
