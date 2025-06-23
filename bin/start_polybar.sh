2c8d46099e3ab8e8efdeff10820c578f000925fc
danielprofili <dprofili@gmail.com>
2025-06-23 16:44:49 -0400

    polybar: fixes

--- bin/start_polybar.sh
+++ bin/start_polybar.sh
@@ -2,7 +2,7 @@ killall -q polybar
 # Start primary monitor bars first so that it gets the tray
 primary=$(polybar --list-monitors | grep "primary" | cut -d ":" -f1)
 MONITOR=$primary polybar --reload topbar &
-MONITOR=$primary polybar --reload wsbar &
+MONITOR=$primary polybar --reload wsbar-primary &
 # Start bars on non primary monitors
 for m in $(polybar --list-monitors | grep -v "primary" | cut -d ":" -f1); do
     MONITOR=$m polybar --reload topbar &
