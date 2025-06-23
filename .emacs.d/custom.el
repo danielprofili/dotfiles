c046fe43d112150c703e81c3dc25d81f0dcaba5d
danielprofili <dprofili@gmail.com>
2025-06-23 16:44:35 -0400

    emacs: modeline

--- .emacs.d/custom.el
+++ .emacs.d/custom.el
@@ -8,7 +8,7 @@
  '(evil-undo-system 'undo-redo)
  '(org-startup-folded t)
  '(package-selected-packages
-   '(modus-themes eaf-browser yasnippet-snippets writeroom-mode which-key telephone-line projectile ox-twbs org-ref org-evil magit ivy highlight-indent-guides helm focus evil-surround evil-better-visual-line disable-mouse dired-git-info delight company benchmark-init backward-forward auctex ample-theme)))
+   '(mood-line modus-themes eaf-browser yasnippet-snippets writeroom-mode which-key telephone-line projectile ox-twbs org-ref org-evil magit ivy highlight-indent-guides helm focus evil-surround evil-better-visual-line disable-mouse dired-git-info delight company benchmark-init backward-forward auctex ample-theme)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
