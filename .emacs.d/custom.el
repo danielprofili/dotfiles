5ae1862e0676023f159d972588c04c253a18aef9
danielprofili <dprofili@gmail.com>
2025-06-25 15:19:29 -0400

    emacs: new stuf

--- .emacs.d/custom.el
+++ .emacs.d/custom.el
@@ -3,17 +3,20 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("75b371fce3c9e6b1482ba10c883e2fb813f2cc1c88be0b8a1099773eb78a7176" "5e39e95c703e17a743fb05a132d727aa1d69d9d2c9cde9353f5350e545c793d4" "77f281064ea1c8b14938866e21c4e51e4168e05db98863bd7430f1352cab294a" "6bf350570e023cd6e5b4337a6571c0325cec3f575963ac7de6832803df4d210a" "a9028cd93db14a5d6cdadba789563cb90a97899c4da7df6f51d58bb390e54031" "e5b6491e99e98d0586766aaf9eb32b1dad3bc3c0c7a1921849d65d3f6d8621a6" default))
  '(dired-kill-when-opening-new-dired-buffer t)
  '(dired-listing-switches "-alF --group-directories-first")
  '(evil-undo-system 'undo-redo)
  '(org-startup-folded t)
  '(package-selected-packages
-   '(markdown-mode mood-line modus-themes eaf-browser yasnippet-snippets writeroom-mode which-key telephone-line projectile ox-twbs org-ref org-evil magit ivy highlight-indent-guides helm focus evil-surround evil-better-visual-line disable-mouse dired-git-info delight company benchmark-init backward-forward auctex ample-theme)))
+   '(expand-region evil-smartparens smartparens smex w3m gruvbox-theme theme-magic xresources-theme markdown-mode mood-line modus-themes eaf-browser yasnippet-snippets writeroom-mode which-key telephone-line projectile ox-twbs org-ref org-evil magit ivy highlight-indent-guides helm focus evil-surround evil-better-visual-line disable-mouse dired-git-info delight company benchmark-init backward-forward auctex ample-theme)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(default: background)
  '(org-document-title ((t (:inherit default :weight bold :height 1.5 :underline nil))))
  '(org-ellipsis ((t (:underline nil))))
  '(org-level-1 ((t (:inherit default :weight bold :height 1.75 :foreground "DarkOrchid1"))))
