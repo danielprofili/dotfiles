88af3a9db73e0345b296d89ea212cacfb787cd3e
danielprofili <dprofili@gmail.com>
2025-06-30 11:39:18 -0400

    emacs: ace window and other stuff

--- .emacs.d/custom.el
+++ .emacs.d/custom.el
@@ -7,6 +7,9 @@
    '("51fa6edfd6c8a4defc2681e4c438caf24908854c12ea12a1fbfd4d055a9647a3" "18a1d83b4e16993189749494d75e6adb0e15452c80c431aca4a867bcc8890ca9" "75b371fce3c9e6b1482ba10c883e2fb813f2cc1c88be0b8a1099773eb78a7176" "5e39e95c703e17a743fb05a132d727aa1d69d9d2c9cde9353f5350e545c793d4" "77f281064ea1c8b14938866e21c4e51e4168e05db98863bd7430f1352cab294a" "6bf350570e023cd6e5b4337a6571c0325cec3f575963ac7de6832803df4d210a" "a9028cd93db14a5d6cdadba789563cb90a97899c4da7df6f51d58bb390e54031" "e5b6491e99e98d0586766aaf9eb32b1dad3bc3c0c7a1921849d65d3f6d8621a6" default))
  '(dired-kill-when-opening-new-dired-buffer t)
  '(dired-listing-switches "-alF --group-directories-first")
+ '(display-time-24hr-format t)
+ '(display-time-day-and-date t)
+ '(display-time-default-load-average nil)
  '(evil-undo-system 'undo-redo)
  '(helm-M-x-show-short-doc t)
  '(helm-completion-style 'emacs)
@@ -14,7 +17,8 @@
  '(helm-source-names-using-follow '("switch-to-buffer" "Buffers"))
  '(org-startup-folded t)
  '(package-selected-packages
-   '(embark-consult orderless consult vertico marginalia embark ox-reveal org-tree-slide epresent expand-region evil-smartparens smartparens smex w3m gruvbox-theme theme-magic xresources-theme markdown-mode mood-line modus-themes eaf-browser yasnippet-snippets writeroom-mode which-key telephone-line projectile ox-twbs org-ref org-evil magit ivy highlight-indent-guides helm focus evil-surround evil-better-visual-line disable-mouse dired-git-info delight company benchmark-init backward-forward auctex ample-theme)))
+   '(ace-window embark-consult orderless consult vertico marginalia embark ox-reveal org-tree-slide epresent expand-region evil-smartparens smartparens smex w3m gruvbox-theme theme-magic xresources-theme markdown-mode mood-line modus-themes eaf-browser yasnippet-snippets writeroom-mode which-key telephone-line projectile ox-twbs org-ref org-evil magit ivy highlight-indent-guides helm focus evil-surround evil-better-visual-line disable-mouse dired-git-info delight company benchmark-init backward-forward auctex ample-theme))
+ '(use-short-answers t))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
