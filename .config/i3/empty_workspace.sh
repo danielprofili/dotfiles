34b56702fe29d32f8aee357792a81c6cc752f2bd
danielprofili <dprofili@gmail.com>
2025-07-04 10:55:35 -0400

    script: improve empty workspace script

--- .config/i3/empty_workspace.sh
+++ .config/i3/empty_workspace.sh
@@ -6,8 +6,21 @@
 # todo: use the smallest available number instead of one larger than
 # the max number currently in use
 
-# some nonsense that gets the workspace number we want
-num=$(i3-msg -t get_workspaces | tr , '\n' | grep '"num":' | cut -d : -f 2 | sort -rn | head -1)
+# list of numbers currently in use
+used=$(i3-msg -t get_workspaces | tr , '\n' | grep '"num":' | cut -d : -f 2 | sort -rn)
+maxnum=$(i3-msg -t get_workspaces | tr , '\n' | grep '"num":' | cut -d : -f 2 | sort -rn | head -1) 
+maxnum=$((maxnum + 1))
+for i in $(seq 1 $maxnum);
+do
+    echo $used | grep -w -q $i
+    if [ $? -eq 1 ]; then
+        num=$i
+        break
+    fi
+done
+echo $i
+# exit 0
+
 
 # necessary if you have non-numbered "pinned" workspaces
 if [ "$num" -eq "-1" ]; then
@@ -15,15 +28,15 @@ if [ "$num" -eq "-1" ]; then
 fi
 
 
-newnum=$(($num + 1))
+# newnum=$(($num + 1))
 
 # new behavior for use with the move-to-named-ws script
 if [[ -n $1 ]]
 then
     # any arg: echo the new number and quit
-    echo $newnum
+    echo $num
 else
     # Creates a new empty workspace
     # no args: regular behavior
-    i3-msg workspace $newnum
+    i3-msg workspace $num
 fi
