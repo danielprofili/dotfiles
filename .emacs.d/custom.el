5eb1f7b3802cf03262833c3c00e51b77c71feafb
danielprofili <dprofili@gmail.com>
2025-08-02 12:37:22 -0400

    picom: opacity rules for stremio

--- .emacs.d/custom.el
+++ .emacs.d/custom.el
@@ -3,7 +3,11 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages nil))
+ '(dired-listing-switches "-alF --group-directories-first")
+ '(evil-undo-system 'undo-redo)
+ '(org-startup-folded t)
+ '(package-selected-packages
+   '(eaf-browser yasnippet-snippets writeroom-mode which-key telephone-line projectile powerline ox-twbs org-ref org-evil magit ivy highlight-indent-guides helm focus evil-surround evil-better-visual-line disable-mouse dired-git-info delight company benchmark-init backward-forward auctex ample-theme)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
